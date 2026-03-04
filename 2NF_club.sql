8119f50093948e39e0644f543553c63c4ba69bfa
srijanstha619-rgb <srijanstha619@gmail.com>
2026-03-04 12:18:47 +0545

    Create 2NF_club.sql

---
+++ 2NF_club.sql
@@ -0,0 +1,8 @@
++--------+-------------+----------+------------+
+| ClubID | ClubName    | ClubRoom | ClubMentor |
++--------+-------------+----------+------------+
+|    101 | Music Club  | R101     | Mr. Raman  |
+|    202 | Sports Club | R202     | Ms. Sita   |
+|    303 | Drama Club  | R303     | Mr. Kiran  |
+|    401 | Coding Club | Lab1     | Mr. Anil   |
++--------+-------------+----------+------------+
