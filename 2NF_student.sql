5b44acd0ac8e14ff1dccf3758eaf18c39a89555f
srijanstha619-rgb <srijanstha619@gmail.com>
2026-03-04 12:15:42 +0545

    Create 2NF_student.sql

---
+++ 2NF_student.sql
@@ -0,0 +1,12 @@
++-----------+-------------+------------------+
+| StudentID | StudentName | Email            |
++-----------+-------------+------------------+
+|         1 | Asha        | asha@email.com   |
+|         2 | Bikash      | bikash@email.com |
+|         3 | Nisha       | nisha@email.com  |
+|         4 | Rohan       | rohan@email.com  |
+|         5 | Suman       | suman@email.com  |
+|         6 | Pooja       | pooja@email.com  |
+|         7 | Aman        | aman@email.com   |
++-----------+-------------+------------------+
+
