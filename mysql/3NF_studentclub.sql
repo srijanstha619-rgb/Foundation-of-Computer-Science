f2d2d651e3ec8b84b9effe73de2e84fb80e98c62
srijanstha619-rgb <srijanstha619@gmail.com>
2026-03-04 12:21:47 +0545

    Create 3NF_studentclub.sql

---
+++ mysql/3NF_studentclub.sql
@@ -0,0 +1,14 @@
++-------------+-------------+------------+
+| StudentName | ClubName    | JoinDate   |
++-------------+-------------+------------+
+| Asha        | Music Club  | 2024-01-10 |
+| Nisha       | Music Club  | 2024-01-20 |
+| Suman       | Music Club  | 2024-01-22 |
+| Bikash      | Sports Club | 2024-01-12 |
+| Asha        | Sports Club | 2024-01-15 |
+| Pooja       | Sports Club | 2024-01-27 |
+| Rohan       | Drama Club  | 2024-01-18 |
+| Bikash      | Drama Club  | 2024-01-25 |
+| Nisha       | Coding Club | 2024-01-28 |
+| Aman        | Coding Club | 2024-01-30 |
++-------------+-------------+------------+
