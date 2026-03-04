e89da7f73c929caf54015d5d7525d9fc193ad17b
srijanstha619-rgb <srijanstha619@gmail.com>
2026-03-04 12:11:08 +0545

    Create 1NF_output.sql

---
+++ 1NF_output.sql
@@ -0,0 +1,14 @@
++-----------+-------------+------------------+-------------+----------+------------+------------+
+| StudentID | StudentName | Email            | ClubName    | ClubRoom | ClubMentor | JoinDate   |
++-----------+-------------+------------------+-------------+----------+------------+------------+
+|         1 | Asha        | asha@email.com   | Music Club  | R101     | Mr. Raman  | 2024-01-10 |
+|         1 | Asha        | asha@email.com   | Sports Club | R202     | Ms. Sita   | 2024-01-15 |
+|         2 | Bikash      | bikash@email.com | Drama Club  | R303     | Mr. Kiran  | 2024-01-25 |
+|         2 | Bikash      | bikash@email.com | Sports Club | R202     | Ms. Sita   | 2024-01-12 |
+|         3 | Nisha       | nisha@email.com  | Coding Club | Lab1     | Mr. Anil   | 2024-01-28 |
+|         3 | Nisha       | nisha@email.com  | Music Club  | R101     | Mr. Raman  | 2024-01-20 |
+|         4 | Rohan       | rohan@email.com  | Drama Club  | R303     | Mr. Kiran  | 2024-01-18 |
+|         5 | Suman       | suman@email.com  | Music Club  | R101     | Mr. Raman  | 2024-01-22 |
+|         6 | Pooja       | pooja@email.com  | Sports Club | R202     | Ms. Sita   | 2024-01-27 |
+|         7 | Aman        | aman@email.com   | Coding Club | Lab1     | Mr. Anil   | 2024-01-30 |
++-----------+-------------+------------------+-------------+----------+------------+------------+
