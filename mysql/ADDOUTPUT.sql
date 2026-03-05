9c29167c224e140a2f85e246d215e91590cb9fb0
srijanstha619-rgb <srijanstha619@gmail.com>
2026-03-05 22:37:02 +0545

    Create ADDOUTPUT.sql

---
+++ mysql/ADDOUTPUT.sql
@@ -0,0 +1,22 @@
++-----------+-------------+------------------+
+| StudentID | StudentName | Email            |
++-----------+-------------+------------------+
+|         1 | Asha        | asha@email.com   |
+|         2 | Bikash      | bikash@email.com |
+|         3 | Nisha       | nisha@email.com  |
+|         4 | Rohan       | rohan@email.com  |
+|         5 | Suman       | suman@email.com  |
+|         6 | Pooja       | pooja@email.com  |
+|         7 | Aman        | aman@email.com   |
+|         9 | Ram         | ram@email.com    |
++-----------+-------------+------------------+
+
++--------+--------------+----------+------------+
+| ClubID | ClubName     | ClubRoom | ClubMentor |
++--------+--------------+----------+------------+
+|    101 | Music Club   | R101     | Mr. Raman  |
+|    202 | Sports Club  | R202     | Ms. Sita   |
+|    303 | Drama Club   | R303     | Mr. Kiran  |
+|    401 | Coding Club  | Lab1     | Mr. Anil   |
+|    501 | Hacking Club | R699     | Ms. Piya   |
++--------+--------------+----------+------------+
